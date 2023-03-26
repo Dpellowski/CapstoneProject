0898e821f1dd4246d91e74abf3f95e8d6c4f7ac9
Mariko16 <86332600+Mariko16@users.noreply.github.com>
2023-03-26 17:48:18 -0500

    login page send user to homepage after logging in

---
+++ login.js
@@ -0,0 +1,39 @@
+let profile;
+let url = "https://2954-2601-444-80-a6c0-3b-41d9-1216-4265.ngrok.io/";
+
+function returnText() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+ 
+  fetch(url + "api/capstone/Login", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+      
+    }),
+  })
+    .then((response) => response.json())
+    .then((response) => (profile = response))
+    .then((response) => loginCheck());
+}
+
+function loginCheck() {
+  if (profile.length > 0) {
+    console.log(profile);
+    if ((profile.accountTypeSID = 2)) {
+      window.location.replace(
+        "http://localhost/ICS499_CapstoneProject/CapstoneProject/index.html"
+      );
+    } else {
+      //send to admin page
+    }
+  } else {
+    //display error on the screen
+    console.log("Fail Login");
+  }
+}
