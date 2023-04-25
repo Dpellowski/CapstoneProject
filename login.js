521491e1bcb53860610f4e25833f9e92b9d02b11
Mariko16 <86332600+Mariko16@users.noreply.github.com>
2023-04-25 17:18:49 -0500

    Login page now redirects to user and admin page

--- login.js
+++ login.js
@@ -5,10 +5,9 @@ function returnText() {
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
 
-  fetch(url + "api/capstone/Login", {
+  fetch(url + 'api/capstone/Login', {
     method: "POST",
     headers: {
-      Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
@@ -17,33 +16,27 @@ function returnText() {
     }),
   })
     .then((response) => response.json())
-    .then((response) => (profile = response))
-    .then((response) => loginCheck());
-}
-
-function loginCheck() {
-  if (profile.length > 0) {
-    // console.log(profile);
-    let profileJson = JSON.stringify(profile);
-    localStorage.setItem("profile", profileJson);
-    console.log(localStorage.getItem("profile"));
-    if ((profile.accountTypeSID == 2)) {
-      window.location.replace(
-        "http://localhost/ICS499_CapstoneProject/CapstoneProject/userProfile.html"
-      );
-    } else if ((profile.accountTypeSID == 1)) {
-      //send to admin page
-      window.location.replace(
-        "http://localhost/ICS499_CapstoneProject/CapstoneProject/adminProfile.html"
-      );
-    } else {
-      //display error on the screen
-      alert("Login Failed!");
-    }
-  } else {
-    //display error on the screen
-    alert("Login Failed!");
-  }
+    .then((profile) => {
+      if (profile.length > 0) {
+        let profileJson = JSON.stringify(profile);
+        localStorage.setItem("profile", profileJson);
+        console.log(profile);
+        if (profile[0].accountTypeSID === 1) {
+          window.location.replace(
+           "http://localhost/ICS499_CapstoneProject/CapstoneProject/adminProfile.html"
+          );
+        } else if (profile[0].accountTypeSID === 2) {
+          window.location.replace(
+            "http://localhost/ICS499_CapstoneProject/CapstoneProject/userProfile.html"
+          );
+        } else {
+          alert("Invalid account type!");
+        }
+      } else {
+        alert("Login Failed!");
+      }
+    })
+    .catch((error) => console.error(error));
 }
 
 
